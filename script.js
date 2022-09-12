e7deb900ffaf06aa59bdc77991324a5f63fd2dc1
Drimor3 <107210912+Drimor3@users.noreply.github.com>
2022-09-12 22:40:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 window.onload = ( () =>{
+
+    let arrmove = [-2, 2]; //некий массив чисел
+    let movesquareY = arrmove[Math.floor(Math.random()*(arrmove.length))];
+
+    let arrmove2 = [-1, 1]; //некий массив чисел
+    let movesquareX = arrmove2[Math.floor(Math.random()*(arrmove2.length))];
+
+
     let speedWall = 3;
     let sppedBallX = 4;
     let sppedBallY = 4;
-    let mover = [];
+    let speedsquare = 5;
+    let moveit = false;
     let i = 0;
-    const screenWidth = window.screen.width - 100;
-    const screenHeight = window.screen.height - 200;
+    let ii = 0;
+    let iii = 0;
+    const screenWidth = 500;
+    const screenHeight = 250;
 
     let ticker = PIXI.Ticker.shared;
     ticker.autoStart = false;
@@ -24,10 +35,22 @@ window.onload = ( () =>{
     wallLeft.height = 100;
     wallLeft.width = 20;
     wallLeft.position.x = 10;
-    wallLeft.position.y = 10;
+    wallLeft.position.y = (screenHeight / 2) - (wallLeft.height / 2);
+
+    let wallRight = new PIXI.Sprite.from('right wall.png')
+    wallRight.height = 100;
+    wallRight.width = 20;
+    wallRight.position.x = screenWidth - wallRight.width - 10 ;
+    wallRight.position.y = screenHeight / 2;
+
+    let square = new PIXI.Sprite.from('square.png')
+    square.height = 30;
+    square.width = 30;
+    square.position.x = screenWidth / 2;
+    square.position.y = screenHeight / 2;
 
     const gameContainer = new PIXI.Container();
-    gameContainer.addChild(wallLeft)
+    gameContainer.addChild(wallLeft, wallRight, square)
     app.stage.addChild(gameContainer)
 
     document.addEventListener('touchend', (george) =>{
@@ -52,5 +75,78 @@ window.onload = ( () =>{
 
     })
 
+// document.addEventListener('keydown', (george) =>{
+//             if(ii === 0){
+//                 ticker.add(()=>{
+//                     wallRight.position.y += sppedBallY;
+//                 })
+//                 ii++
+//             }
+//             else if(ii > 0){
+//                 ticker.add(()=>{
+//                     wallRight.position.y -= (sppedBallY * 2);
+//                 })
+//                 ii -=2
+//             }
+//             else if (ii<0){
+//                 ticker.add(()=>{
+//                     wallRight.position.y += (sppedBallY * 2);
+//                 })
+//                 ii +=2
+//             }
+//
+//     })
 
+    // тікер начала move it move it квадрата, і його зміна швидкості при ударяння в стенкі
+    ticker.add(()=>{
+    if(iii === 0){
+        iii++
+        ticker.add(()=>{
+            square.position.x -= speedsquare * movesquareX;
+            square.position.y += movesquareY;
+        })
+    }
+    else if(wallLeft.position.x + wallLeft.width > square.position.x
+        &&
+        wallLeft.position.y + wallLeft.height > square.position.y
+        &&
+        wallLeft.position.y < square.position.y + square.width
+        &&
+        wallLeft.position.x < square.position.x + square.height)
+    {
+        ticker.add(()=>{
+            square.position.x += speedsquare * 2
+        })
+    }
+    else if(wallRight.position.x + wallRight.width > square.position.x
+        &&
+        wallRight.position.y + wallRight.height > square.position.y
+        &&
+        wallRight.position.y < square.position.y + square.width
+        &&
+        wallRight.position.x < square.position.x + square.height
+    ){
+        ticker.add(()=>{
+            square.position.x -= speedsquare * 2
+        })
+    }
+    })
+
+    ticker.add(()=>{
+        wallRight.position.y = square.position.y
+        if(square.position.y > screenHeight - square.height){
+            ticker.add(()=>{
+                square.position.y += movesquareY * -1
+            })
+        }
+        else if(square.position.y < 0){
+            ticker.add(()=>{
+                square.position.y += movesquareY
+            })
+
+        }
+    })
+    // document.addEventListener('keydown', ()=>{
+    //     console.log(square.position.y)
+    // })
 })
